89c896dd0c4f0e0dfbeba80ca7a1196c6882049d
Your Name <you@example.com>
2022-08-29 13:37:31 +0530

    added:Day3

---
+++ src/DeclareArray.java
@@ -0,0 +1,9 @@
+public class DeclareArray {
+        public static void main(String[] args) {
+            int[] arr = new int [] {1,2,3,4,5};
+            System.out.println("elements of array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+}
